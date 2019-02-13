13ff1a5ac4e64da9bc5bfa847897fa49a6a7de50
Vadiraj Bhatt <bhatt.vadiraj@gmail.com>
2019-02-13 19:46:07 +0530

    Upadated Dockerfile

--- initdb-pgjwt.sh
+++ initdb-pgjwt.sh
@@ -5,9 +5,14 @@ set -e
 # Perform all actions as $POSTGRES_USER
 export PGUSER="$POSTGRES_USER"
 
+# Create the 'template_pgjwt' template db
+"${psql[@]}" <<- 'EOSQL'
+CREATE DATABASE template_pgjwt;
+UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_pgjwt';
+EOSQL
 
-# Load PostGIS into both template_database and $POSTGRES_DB
-for DB in "$POSTGRES_DB"; do
+# Load pgjwt into both template_database and $POSTGRES_DB
+for DB in template_pgjwt "$POSTGRES_DB"; do
 	echo "Loading pgjwt & pgagent extensions into $DB"
 	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
 		CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA $POSTGRES_EXTENSION_SCHEMA;
